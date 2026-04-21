0189eab6d1bc35ede71a1d11c6ac7e2ade7becab
Promise <i@xenlyra.cc>
2026-04-22 00:03:53 +0800

    update deps

--- scripts/typegen.ts
+++ scripts/typegen.ts
@@ -1,8 +1,7 @@
-import { Linter } from 'eslint'
 import typegen from 'eslint-typegen'
 
 import promise from '../src'
 
 const configs = promise({ enable: { ts: true, vue: true } })
 
-await typegen(configs as Linter.Config[], { dtsPath: 'src/typegen.d.ts' })
+await typegen(configs, { dtsPath: 'src/typegen.d.ts' })
